85ecdb8a440e13dfea621a164ac1d6d703de3a1e
mj-grc <armour-johnson.michael@student.greenriver.edu>
2026-07-11 14:21:45 -0700

    just some updates

---
+++ Assignments/michael_armour_johnson_assignment1.py
@@ -0,0 +1,5 @@
+name  = str(input("What is your name? "))
+print("Hello ", name, " Today is going to be a great day!")
+age = int(input("What's your age? "))
+print("Wow", age , "is a good age to be")
+
