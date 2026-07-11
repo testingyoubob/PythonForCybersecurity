f2540f7a6ef6026c55d9c458bb13d4e4edf6b367
mj-grc <armour-johnson.michael@student.greenriver.edu>
2026-07-11 13:35:24 -0700

    Finishing up on more work and all of the ch2

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 # A more complex "Hello World" script in python with Inputs
 # Created 
+name = input("What's your name? ")
+print("Hello", name)
+age = int(input("What is your age? "))
+print("And you are", age, "old wow cool")
--- start/CH02/SimpleCalculator.py
+++ start/CH02/SimpleCalculator.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 # A simple calculator to show math and conditionals
 # Created 
+mathNumbersOne = 0
+mathNumbersTwo = 0 
+
+mathNumbersOne = float(input("Enter your first number "))
+
+mathNumbersTwo = float(input("Enter your second number "))
+print("\n")
+print("Going to add those numbers  ", mathNumbersOne + mathNumbersTwo)
+print("Going to Subtrack those numbers ", mathNumbersOne - mathNumbersTwo)
+print("Going to Multiply those numbers ", mathNumbersOne * mathNumbersTwo)
+print("Going to devide those numbers ", mathNumbersOne/ mathNumbersTwo)
+print("\n")
