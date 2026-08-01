501bfecb759c37475f82fe8668e9daf9659fc759
mj-grc <armour-johnson.michael@student.greenriver.edu>
2026-08-01 13:52:58 -0700

    small text changes

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
 # By 
+
+# f = open("test.txt", "r")
+# read = f.read()
+# print(read)
+# f.close()
+
+# get the current file
+import os 
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
+
+#create the file path 
+
+file_path = os.path.join(script_dir, "test.txt")
+#open files for reading by using the 'r' 
+
+f = open(file_path, "r")
+
+#read contents to fhe file 
+file_content = f.read()
+print(file_content) 
+
+f.close()   
--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
 # By 
+
+#Open file for writing - f is used. 
+firstName = input("What is your name ")
+f = open ("test.txt", "w")
+
+#write to file
+f.write(firstName + ", Nice to meet you")
+
+#close the files. 
+f.close()
+print("file closed")
+#Want to take a look inside before closing for good
+f = open("test.txt", "r")
+print(f.read())
+f.close()
